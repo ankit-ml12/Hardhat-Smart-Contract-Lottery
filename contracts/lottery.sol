5c2d8278dbe460ff9a8886cab2be5aa9e16bd9aa
ankit patidar <patiankit6555@gmail.com>
2022-12-10 15:48:26 +0530

    2hl

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -1,29 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.7;
 
-Lottery_NotEnoughETHEntered();
-contract Lotter{
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+error Lottery_NotEnoughETHEntered();
+contract Lotter is VRFConsumerBaseV2{
     uint immutable i_entranceFee;
     address payable[] private s_players;
 
     event  LotteryEnter(address indexed player);
 
-    constructor (uint entranceFee){
+    constructor (address vrfCoordinatorV2, uint entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2){
         i_entranceFee = entranceFee;
     }
 
     function  enterLotter()public payable {
         if(msg.value<i_entranceFee){
-            revert Lottery_NotEnoughETHEntered()
+            revert Lottery_NotEnoughETHEntered();
         }
         s_players.push(payable(msg.sender));
         emit LotteryEnter(msg.sender);
     }
 
+    function requestRandomWinner() external {
+
+    }
+    function fulfillRandomWords(uint requestId, uint [] memory randomWords) internal override{}
+
     function getEntranceFee()public view returns (uint){
         return i_entranceFee;
     }
-    function getPlayer(uint index)public view returns (uint){
+    function getPlayer(uint index)public view returns (address){
         return s_players[index];
     }
 
